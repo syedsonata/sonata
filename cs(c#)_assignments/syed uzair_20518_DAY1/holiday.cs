1ea7a87f99e9f1d279893e1a7db920126d1e216a
syedsonata <syed.razvi@sonata-software.com>
2021-09-29 01:52:49 +0530

    c# assignment for day 1

---
+++ cs(c#)_assignments/syed uzair_20518_DAY1/holiday.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VsHelloworld
+{
+    class holiday
+    {
+        static void Main()
+        {
+            Console.WriteLine("enter the day");
+            string day = Console.ReadLine();
+            int n = int.Parse(day);
+            if(n==0 || n > 7)
+            {
+                Console.WriteLine("invalid day");
+            }else if (n <= 5)
+            {
+                Console.WriteLine("working day");
+            }else
+                Console.WriteLine("holiday");
+        }
+    }
+}
---
+++ cs(c#)_assignments/syed uzair_20518_DAY1/primeIndex.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VsHelloworld
+{
+    class primeIndex
+    {
+		static void Main(string[] args)
+		{
+			int i, j, p, y = 0;
+			int[] arr = new int[20];
+			int[] newarr = new int[20];
+			Console.WriteLine("enter the number of elements");
+			int n = int.Parse(Console.ReadLine());
+			Console.WriteLine("Enter array elements:(after every element give enter and proced)");
+			for (i = 0; i < n; i++)
+			{
+				arr[i] = int.Parse(Console.ReadLine());
+			}
+
+			for (i = 0; i < n; i++)
+			{
+				j = 2;
+				p = 1;
+				while (j < arr[i])
+				{
+					if (arr[i] % j == 0)
+					{
+						p = 0;
+						break;
+					}
+					j++;
+				}
+
+				if (p == 1)
+				{
+					newarr[y] = arr[i];
+					//Console.WriteLine(newarr[y]);
+					y++;
+				}
+			}
+			int number = newarr.Max();
+			int numb1 = Array.IndexOf(arr, number);
+			Console.WriteLine(number + " is the biggest prime number and the index value is " + numb1);
+		}
+	}
+}
