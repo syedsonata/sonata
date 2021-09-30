dcb1f47261cf3f9b81bcdb0e7d7ce50ca3df54a9
syedsonata <syed.razvi@sonata-software.com>
2021-09-30 15:36:39 +0530

    day 2 assignment c#

---
+++ cs(c#)_assignments/syed uzair_20518_DAY2/circle.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VsHelloworld
+{
+    public class Circle
+    {
+
+        private double radius;
+        private double PI = 3.14159;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+
+        public Circle()
+        {
+            radius = 0.0;
+        }
+
+        public void setRadius(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double getRadius()
+        {
+            return radius;
+        }
+
+
+        public double getArea()
+        {
+            return PI * radius * radius;
+        }
+
+        public double getDiameter()
+        {
+            return radius * 2;
+        }
+
+
+        public double getCircumference()
+        {
+            return 2 * PI * radius;
+        }
+    }
+    class circle1
+    {
+        public static void Main(String[] args)
+        {
+
+            double radius;
+            Console.WriteLine("Please enter the circle's radius");
+            radius = double.Parse(Console.ReadLine());
+
+            Circle c1 = new Circle(radius);
+
+            Console.WriteLine("The area of the circle with radius of " + radius + " is " + c1.getArea()
+                    + " with diameter of " + c1.getDiameter() + " and with circumference of " + c1.getCircumference());
+        }
+    }
+}
+
