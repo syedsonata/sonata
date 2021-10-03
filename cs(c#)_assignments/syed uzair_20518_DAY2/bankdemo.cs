34dc6939432b572ef73a5f0a0d9d469b9f1cc8ec
syedsonata <syed.razvi@sonata-software.com>
2021-10-03 23:45:26 +0530

    Assignment day2

---
+++ cs(c#)_assignments/syed uzair_20518_DAY2/bankdemo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops_concepts
+{
+    public class bank
+    {
+        string name;
+        int accno;
+        int balance;
+        public bank(string nam ,int acc)
+        {
+            name = nam;
+            accno = acc;
+            balance = 500;
+        }
+        public bank(string nam, int acc, int bal)
+        {
+            name = nam;
+            accno = acc;
+            balance = bal;
+        }
+        public string CustName()
+        {
+            return name;
+        }
+        public int AccNO()
+        {
+            return accno;
+        }
+        public int CustBal()
+        {
+            return balance;
+        }
+
+
+    }
+    class bankdemo
+    {
+        public static void Main1()
+        {
+            bank b1 = new bank("syed", 100);
+            Console.WriteLine("for example name is given as syed and account number as 100 and the balance is not given then ");
+            Console.WriteLine("name-"+b1.CustName()+" account number-"+b1.AccNO()+" balance-"+b1.CustBal());
+            Console.WriteLine("second example will be user input");
+            Console.WriteLine("Please enter the name");
+            string n = Console.ReadLine();
+            Console.WriteLine("Please enter the Account number");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter balance");
+            int b = int.Parse(Console.ReadLine());
+            bank b2 = new bank(n,a,b);
+            Console.WriteLine("name-" + b2.CustName() + " account number-" + b2.AccNO() + " balance-" + b2.CustBal());
+
+        }
+    }
+}
---
+++ cs(c#)_assignments/syed uzair_20518_DAY2/emp.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops_concepts
+{
+    public class EmpSalary
+    {
+        string name;
+        int eid;
+        int Bsalary;
+        int hr;
+        int trvlall;
+
+        public EmpSalary(string name, int eid, int Bsalry, int hr, int trvlall)
+        {
+            this.name = name;
+            this.eid = eid;
+            this.Bsalary = Bsalry;
+            this.hr = hr;
+            this.trvlall = trvlall;
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public int geteid()
+        {
+            return eid;
+        }
+        public int getBasic()
+        {
+            return Bsalary;
+        }
+        public int getHr()
+        {
+            return Bsalary;
+        }
+        public int getTrvl()
+        {
+            return Bsalary;
+        }
+        public double tax()
+        {
+            if (Bsalary * 12 < 50000 && Bsalary * 12 > 0)
+                return Bsalary * 0;
+            else if (Bsalary * 12 > 50000 && Bsalary * 12 < 70000)
+                return Bsalary * .15;
+            else
+                return Bsalary * .2;
+        }
+        public int gross()
+        {
+            return Bsalary + hr + trvlall;
+        }
+        }
+        class emp
+    {
+        static void Main(string[] args)
+        {
+            EmpSalary emp1 = new EmpSalary("syed", 20792, 6000, 1000, 500);
+
+            Console.WriteLine("Hello" + emp1.getName() + ".\n Emp ID:" + emp1.geteid() + ".\n Basic salary:" + emp1.getBasic() + ".\n HR Allowance:" + emp1.getHr() + ".\n Travel Allowance:" + emp1.getTrvl()+".\n Tax:" + emp1.tax() + ".\n Gross" + emp1.gross());
+        }
+    }
+}
